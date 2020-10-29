d4944d2e4e6be9dbd4cdd575e93a3a8dfefbee57
puge <mail@puge.org>
2020-10-30 00:09:12 +0800

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,34 @@
+function scrollIntoView () {
+  window.scrollTo(0, 0)
+}
+
+// 阻止微信拖动
+document.body.addEventListener('touchmove', function (e) {
+  e.preventDefault() // 阻止默认的处理方式(阻止下拉滑动的效果)
+}, {passive: false})
+
+setTimeout(() => {
+  autoScale({
+    deviseW: 750,
+    deviseH: 1508,
+    center: 'middle',
+    scroll: false,
+    type: 'scale',
+    box: '.scale-box'
+  })
+}, 100);
+
+//生成从minNum到maxNum的随机数
+function randomNum(minNum,maxNum){ 
+  switch(arguments.length){ 
+      case 1: 
+          return parseInt(Math.random()*minNum+1,10); 
+      break; 
+      case 2: 
+          return parseInt(Math.random()*(maxNum-minNum+1)+minNum,10); 
+      break; 
+          default: 
+              return 0; 
+          break; 
+  } 
+} 
